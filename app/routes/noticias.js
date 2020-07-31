fcdce6aa0be631c22ed68f6b12e4b398cd19378c
Ismael Santos <ismael496@yahoo.com.br>
2020-07-31 10:11:38 -0300

    Refactoring com Consing (no bd)

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,12 +1,16 @@
-const dbConnetcion = require('../../config/dbConnection');
+
 module.exports = function(app){
-    const connection = dbConnetcion();
+
+    
 
     app.get('/noticias', function(req, res){
+
+        const connection = app.config.dbConnection();
 
         connection.query('select * from noticias', function(err, result) {
             res.render("noticias/noticias",{noticias: result});  
         });
     });
 };
+
 
--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql');
 
-module.exports = function() {
-    return mysql.createConnection({
+const connMySQL = function(){
+        console.log('Conexão com bd foi estabelecida')
+        return mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: '34837137',
         database: 'portal_noticias'
     });
+}
+module.exports = function() {
+    
+    return connMySQL;
 }
