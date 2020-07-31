28622e472661960086c767209c8990eca93b7b48
Ismael Santos <ismael496@yahoo.com.br>
2020-07-31 09:33:41 -0300

    Começando a commitar

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,6 +1,12 @@
+const dbConnetcion = require('../../config/dbConnection');
 module.exports = function(app){
+    const connection = dbConnetcion();
+
     app.get('/noticias', function(req, res){
-    res.render("noticias/noticias")
-});
-}
+
+        connection.query('select * from noticias', function(err, result) {
+            res.render("noticias/noticias",{noticias: result});  
+        });
+    });
+};
 
