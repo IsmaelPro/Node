93c6f86acd6faec7ee1434a59833163492d87768
Ismael Santos <ismael496@yahoo.com.br>
2020-07-31 10:33:20 -0300

    Criando a página de detalhes das noticias

---
+++ app/routes/noticia.js
@@ -0,0 +1,12 @@
+
+module.exports = function(app){
+
+    app.get('/noticia', function(req, res){
+
+        const connection = app.config.dbConnection();
+
+        connection.query('select * from noticias where id_noticia = 2', function(err, result) {
+            res.render("noticias/noticia",{noticia: result});  
+        });
+    });
+};
