a997bfb07c82c2e4f109253a8b463ec10c068b61
Ismael Santos <ismael496@yahoo.com.br>
2020-08-05 10:48:28 -0300

    Aplicando logica no template novo

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,12 @@
 module.exports.index = function(application, req, res){
-    res.render("home/index")
+    const connection = application.config.dbConnection();
+    const noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+    
+
+    
+    noticiasModel.get5TultimasNoticias(function(err, result){
+    
+        res.render("home/index", {noticias: result});
+    });
 }
--- app/controllers/noticias.js
+++ app/controllers/noticias.js
@@ -9,9 +9,11 @@ module.exports.noticias = function(application, req, res){
 
 module.exports.noticia = function(application, req, res){
     const connection = application.config.dbConnection();
-        const noticiasModel = new application.app.models.NoticiasDAO(connection);
+    const noticiasModel = new application.app.models.NoticiasDAO(connection);
+    const id_noticia = req.query;
+   
 
-        noticiasModel.getNoticia(function(err, result) {
-            res.render("noticias/noticia",{noticia: result});  
-        });
+    noticiasModel.getNoticia(id_noticia, function(err, result) {
+        res.render("noticias/noticia",{noticia: result});  
+    });
 }
